812a9f7e82856c726119e455d665a0bc6018df96
chapulina <5751272+chapulina@users.noreply.github.com>
2015-09-21 11:13:21 -0700

    doc

---
+++ setup.sh
@@ -0,0 +1,3 @@
+export GAZEBO_PLUGIN_PATH=/home/louise/code/cppcon/camera_poses/build:/home/louise/code/cppcon/animated_quad/build:$GAZEBO_PLUGIN_PATH
+
+export GAZEBO_MODEL_PATH=/home/louise/code/cppcon/world/models:$GAZEBO_MODEL_PATH
