e76501fc024e1d36888ce7dc344d8c8a720bc192
chapulina <burajiru.no.chapulina@gmail.com>
2016-05-21 18:26:41 -0700

    generic instructions

--- build_plugins.sh
+++ build_plugins.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo "Compiling Camera Poses Plugin"
-cd camera_poses
+echo -e "\e[101mCompiling Camera Poses Plugin\e[49m"
+cd plugins/camera_poses
 rm -rf build
 mkdir build
 cd build
 cmake ..
 make -j4
 export GAZEBO_PLUGIN_PATH=`pwd`:$GAZEBO_PLUGIN_PATH
-cd ../../
+cd ../../../
 
-echo "Compiling Animate Model Plugin"
-cd animate_model
+echo -e "\e[101mCompiling Animate Model Plugin\e[49m"
+cd plugins/animate_model
 rm -rf build
 mkdir build
 cd build
 cmake ..
 make -j4
 export GAZEBO_PLUGIN_PATH=`pwd`:$GAZEBO_PLUGIN_PATH
-cd ../../
+cd ../../../
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export GAZEBO_PLUGIN_PATH=$DIR/camera_poses/build:$DIR/animate_model/build:$GAZEBO_PLUGIN_PATH
+export GAZEBO_PLUGIN_PATH=$DIR/plugins/camera_poses/build:$DIR/plugins/animate_model/build:$GAZEBO_PLUGIN_PATH
 
 export GAZEBO_MODEL_PATH=$DIR/world/models:$GAZEBO_MODEL_PATH
