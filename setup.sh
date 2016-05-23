72cc529e099891e1fa820efc4a694d950f7f36f9
chapulina <5751272+chapulina@users.noreply.github.com>
2016-05-22 22:19:54 -0700

    new slides, better quad motion, thank you

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GAZEBO_PLUGIN_PATH=$DIR/plugins/camera_poses/build:$DIR/plugins/animate_model/build:$GAZEBO_PLUGIN_PATH
 
-export GAZEBO_MODEL_PATH=$DIR/world/models:$GAZEBO_MODEL_PATH
+export GAZEBO_MODEL_PATH=$DIR/world/models:$DIR/models:$GAZEBO_MODEL_PATH
