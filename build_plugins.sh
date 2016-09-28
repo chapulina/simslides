462917bd72aef2cc1b1800be5e69517b5c0cbbdf
chapulina <5751272+chapulina@users.noreply.github.com>
2016-09-27 17:27:29 -0700

    support trusty, remove old bash files, add new bash files

--- build_plugins.sh
+++ build_plugins.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-echo -e "\e[101mCompiling Camera Poses Plugin\e[49m"
-cd plugins/camera_poses
+cd plugins
 rm -rf build
 mkdir build
 cd build
 cmake ..
 make -j4
 export GAZEBO_PLUGIN_PATH=`pwd`:$GAZEBO_PLUGIN_PATH
-cd ../../../
-
-echo -e "\e[101mCompiling Animate Model Plugin\e[49m"
-cd plugins/animate_model
-rm -rf build
-mkdir build
-cd build
-cmake ..
-make -j4
-export GAZEBO_PLUGIN_PATH=`pwd`:$GAZEBO_PLUGIN_PATH
-cd ../../../
+cd ../../
--- prepare_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -e "\e[101mBuilding plugins...\e[49m"
-sh build_plugins.sh
-
-echo -e "\e[101mGenerating slide world...\e[49m"
-bash generate_models.sh stack
