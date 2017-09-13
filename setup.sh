7b4363fe734e62c9329560c911ae1158b71de435
chapulina <burajiru.no.chapulina@gmail.com>
2017-09-12 18:12:06 -0700

    untested readme

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GAZEBO_PLUGIN_PATH=$DIR/plugins/build:$DIR/plugins/reset_model/build:/usr/local/lib/gazebo-8/plugins/:/usr/local/lib/x86_64-linux-gnu/gazebo-8/plugins/:$DIR:$GAZEBO_PLUGIN_PATH
 
-export GAZEBO_MODEL_PATH=$DIR/worlds/models:$DIR/models:$DIR/../srcsim/models:$GAZEBO_MODEL_PATH
+export GAZEBO_MODEL_PATH=$DIR/worlds/models:$DIR/models:$DIR/../srcsim/models:$DIR/../val_gazebo_models:$GAZEBO_MODEL_PATH
