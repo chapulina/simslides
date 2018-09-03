16dc7cd96b23d656680a1dd1d8cb92d799b67463
chapulina <burajiru.no.chapulina@gmail.com>
2018-09-03 08:55:42 -0700

    copyright notices

--- src/Keyframe.hh
+++ src/Keyframe.hh
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Louise Poubel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 #ifndef SIMSLIDES_KEYFRAME_HH_
 #define SIMSLIDES_KEYFRAME_HH_
 
@@ -14,8 +29,14 @@ namespace simslides
   /// \brief Keyframe types
   enum KeyframeType
   {
+    /// \brief Move the camera to look at a given slide
     LOOKAT,
+
+    /// \brief The slide is part of a stack. Slides on a stack are located in
+    /// the same position.
     STACK,
+
+    /// \brief Seek to a specific time in a log file.
     LOG_SEEK
   };
 
