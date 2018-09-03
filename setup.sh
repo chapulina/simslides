cea86042e539951283d7788c3c287d5f21e05aa6
chapulina <5751272+chapulina@users.noreply.github.com>
2018-09-03 08:38:42 -0700

    cleanup cmake

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export GAZEBO_PLUGIN_PATH=$DIR/plugins/build:$DIR/plugins/reset_model/build:/usr/local/lib/gazebo-9/plugins/:/usr/local/lib/x86_64-linux-gnu/gazebo-9/plugins/:$GAZEBO_PLUGIN_PATH
+export GAZEBO_PLUGIN_PATH=$DIR/build/src:$DIR/build/src:/usr/local/lib/gazebo-9/plugins/:/usr/local/lib/x86_64-linux-gnu/gazebo-9/plugins/:$GAZEBO_PLUGIN_PATH
 
 export GAZEBO_MODEL_PATH=$DIR/worlds/models:$DIR/models:$GAZEBO_MODEL_PATH
