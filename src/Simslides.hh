83c790f53b161912c98f1934c5546573c848fbe8
chapulina <5751272+chapulina@users.noreply.github.com>
2018-09-03 08:55:42 -0700

    copyright notices

--- src/Simslides.hh
+++ src/Simslides.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Open Source Robotics Foundation
+ * Copyright 2016 Louise Poubel
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
 */
 #ifndef SIMSLIDES_SIMSLIDES_HH_
 #define SIMSLIDES_SIMSLIDES_HH_
